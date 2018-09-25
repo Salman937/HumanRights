0f854ddfe9bd5e54313da13f1a07edd0ca324455
Salman-Iqbal <si87841@gmail.com>
2018-09-25 10:44:10 +0500

    announcement table migration created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(ComplaintStatusTableSeeder::class);
         $this->call(AwarenessTableSeeder::class);
+        $this->call(AnnouncementsTableSeeder::class);
     }
 }
